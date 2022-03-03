bf575fd8aa4451dca7539d37567c5d8468d9b70c
sarangcoding <pinklybarbie@gmail.com>
2022-03-03 18:37:52 +0900

    method practice

--- Challenge.java
+++ Challenge.java
@@ -1,35 +1,29 @@
 public class Challenge {
     public static void main(String[] args)
-    {   boolean newGameOver = true;
-        int newScore = 10000;
-        int newLevelCompleted = 8;
-        int newBonus = 200;
+    {  
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        // if(score < 5000 && score > 1000) {
-        //     System.out.println("Your score was less than 5000 but greater than 1000");
-        // } 
-        // else if(score < 1000){
-        //     System.out.println("Your score was less than 1000");
-        // }
-        // else{ 
-        //     System.out.println("Got here");
-        // }
-        //test comment
-     if(newGameOver) {
-         int finalScore = newScore + (newLevelCompleted * newBonus);
-         finalScore += 1000;
-         System.out.println("Your final score was " + finalScore);
-     }
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
-     //Print out a second score on the screen with the following
-     //score set to 10000
-     //levelCompleted set to 8
-     //bonus set to 200
-     //But make sure the first printout above still displays as well 
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
 
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+    }
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+      
     
-
-
-
+     if(gameOver) {
+         int finalScore = score + (levelCompleted * bonus);
+         finalScore += 2000;
+         System.out.println("Your final score was " + finalScore);
+         return finalScore ; 
+     }
+     return -1;
     }
 }
